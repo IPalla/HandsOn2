1142eef7a2ac8f1b92e5b83077d6460e693ddde8
Ignacio Pallares Jimenez <ignacio.pallares@babel.es>
2018-03-16 09:47:13 +0100

    Aside terminado

--- HandsOn2/src/app/estaciones/estaciones.module.ts
+++ HandsOn2/src/app/estaciones/estaciones.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { EstacionesRoutingModule } from './estaciones-routing.module';
 import { EstacionesComponent } from './estaciones.component';
 import { EstacionEstandarComponent } from './estacion-estandar/estacion-estandar.component';
+import { AsideComponent } from '../core/aside/aside.component';
+import { CoreModule } from '../core/core.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    EstacionesRoutingModule
+    EstacionesRoutingModule, CoreModule
   ],
   declarations: [EstacionesComponent, EstacionEstandarComponent],
   exports: [EstacionesComponent]
